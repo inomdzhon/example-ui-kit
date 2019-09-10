8b3ad786802908d86b6ccd539990a97b0e049273
i.mirdzhamolov <i.mirdzhamolov@space307.com>
2019-09-10 17:50:00 +0300

    Updates

---
+++ precache-manifest.fd0054c5d3c0cc6e2ea069221f2bb042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5664b43c2237d7509c89",
+    "url": "/example-ui-kit/static/js/runtime~main.ed0cb2aa.js"
+  },
+  {
+    "revision": "545ea9667c76ecd6ce6f",
+    "url": "/example-ui-kit/static/js/main.c3202430.chunk.js"
+  },
+  {
+    "revision": "6d114db069674734812d",
+    "url": "/example-ui-kit/static/js/2.4401721c.chunk.js"
+  },
+  {
+    "revision": "545ea9667c76ecd6ce6f",
+    "url": "/example-ui-kit/static/css/main.f6cdfc3e.chunk.css"
+  },
+  {
+    "revision": "449348e1b84669fb0688df505f630ec2",
+    "url": "/example-ui-kit/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/example-ui-kit/precache-manifest.29befec94dfa0023f231975b15fb32c1.js"
+  "/example-ui-kit/precache-manifest.fd0054c5d3c0cc6e2ea069221f2bb042.js"
 );
 
 workbox.clientsClaim();
