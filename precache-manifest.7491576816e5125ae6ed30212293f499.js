edf481e826e097a8145ec12fc648dc6c192677ed
i.mirdzhamolov <i.mirdzhamolov@space307.com>
2019-09-10 15:56:34 +0300

    Updates

---
+++ precache-manifest.7491576816e5125ae6ed30212293f499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc0889df0451f087f85c",
+    "url": "/example-ui-kit/static/js/runtime~main.bc0889df.js"
+  },
+  {
+    "revision": "5eac10bb2944f16c9d20",
+    "url": "/example-ui-kit/static/js/main.5eac10bb.chunk.js"
+  },
+  {
+    "revision": "25e7a2d8c0758ef72459",
+    "url": "/example-ui-kit/static/js/1.25e7a2d8.chunk.js"
+  },
+  {
+    "revision": "5eac10bb2944f16c9d20",
+    "url": "/example-ui-kit/static/css/main.12709616.chunk.css"
+  },
+  {
+    "revision": "124c7c5cc0bc2df5ff797978d483eece",
+    "url": "/example-ui-kit/index.html"
+  }
+];
