7d1c55ddb1df90b01466b623cd36c9d184d034f6
i.mirdzhamolov <i.mirdzhamolov@space307.com>
2019-09-17 19:25:28 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/example-ui-kit/precache-manifest.a2e3fc570e317aa337af02a91b7324fd.js"
+  "/example-ui-kit/precache-manifest.f1224e395c52d9aebfd80761f2f5a726.js"
 );
 
 workbox.clientsClaim();
